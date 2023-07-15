6670e0d3542363979d05aaa5fedbe114f8088a46
YesunPark <lynn08082@gmail.com>
2023-07-16 00:51:38 +0900

    Fix: AppConfig 파일에 @Configuration 누락된 것 추가

--- src/main/java/com/zerobase/BankSSun/config/AppConfig.java
+++ src/main/java/com/zerobase/BankSSun/config/AppConfig.java
@@ -1,9 +1,11 @@
 package com.zerobase.BankSSun.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class AppConfig {
 
   @Bean
