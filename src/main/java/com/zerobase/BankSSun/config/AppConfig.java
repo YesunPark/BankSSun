29a6a1be1b1b0fe89a736b995f08562ba2b8c7ac
YesunPark <lynn08082@gmail.com>
2023-07-23 02:21:32 +0900

    Feat: AppConfig, JpaAuditingConfig 파일 작성

--- src/main/java/com/zerobase/BankSSun/config/AppConfig.java
+++ src/main/java/com/zerobase/BankSSun/config/AppConfig.java
@@ -2,10 +2,12 @@ package com.zerobase.BankSSun.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity // SpringSecurity 활성화
 public class AppConfig {
 
   @Bean
