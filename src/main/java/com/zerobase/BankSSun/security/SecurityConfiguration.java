3c19567de0e718c48783a1d85c4dceb46ae43c64
YesunPark <lynn08082@gmail.com>
2023-07-23 02:20:42 +0900

    Feat: SecurityConfiguration 파일 작성

--- src/main/java/com/zerobase/BankSSun/security/SecurityConfiguration.java
+++ src/main/java/com/zerobase/BankSSun/security/SecurityConfiguration.java
@@ -1,5 +1,48 @@
 package com.zerobase.BankSSun.security;
 
-public class SecurityConfiguration {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Slf4j
+@Configuration
+@EnableWebSecurity
+// @EnableGlobalMethodSecurity(prePostEnabled = true) -> 권한정보를 제어하고 싶으면
+// 실제로 제어할 곳에서 @PreAuthorize 을 통해 제어하기 위한 어노테이션. 현재는 불필요해서 주석
+@RequiredArgsConstructor
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final JwtAuthenticationFilter authenticationFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/**/sign-up", "/**/sign-in").permitAll()
+            .and()
+            .addFilterBefore(this.authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(final WebSecurity web) throws Exception {
+        web.ignoring()
+            .antMatchers("/h2-console/**");
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }
