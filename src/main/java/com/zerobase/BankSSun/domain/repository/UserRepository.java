d08636d4b82e6138574bdfaaf1878bb8351baaf4
YesunPark <lynn08082@gmail.com>
2023-07-25 09:47:01 +0900

    Refactor: 변수명 첫글자 소문자로 수정

--- src/main/java/com/zerobase/BankSSun/domain/repository/UserRepository.java
+++ src/main/java/com/zerobase/BankSSun/domain/repository/UserRepository.java
@@ -10,5 +10,5 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByPhone(String phone); // 이메일을 기준으로 회원 정보 찾음
 
-    boolean existsByPhone(String Phone); // 회원가입 시 이미 존재하는 회원인지 확인
+    boolean existsByPhone(String phone); // 회원가입 시 이미 존재하는 회원인지 확인
 }
