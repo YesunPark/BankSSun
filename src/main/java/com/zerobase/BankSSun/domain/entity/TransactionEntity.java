30f8ad7584cd55f528075dbbcbf4e2747f3c049a
YesunPark <lynn08082@gmail.com>
2023-08-02 20:06:42 +0900

    Refactor: 사용하지 않는 import문 제거

--- src/main/java/com/zerobase/BankSSun/domain/entity/TransactionEntity.java
+++ src/main/java/com/zerobase/BankSSun/domain/entity/TransactionEntity.java
@@ -10,7 +10,6 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
