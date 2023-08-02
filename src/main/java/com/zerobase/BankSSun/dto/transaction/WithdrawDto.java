bfa050b68f4bbc29d2f9253ff46fc8f1d0310b49
YesunPark <lynn08082@gmail.com>
2023-08-02 15:22:51 +0900

    Fix: 현금 출금 Request 에러메세지 추가

--- src/main/java/com/zerobase/BankSSun/dto/transaction/WithdrawDto.java
+++ src/main/java/com/zerobase/BankSSun/dto/transaction/WithdrawDto.java
@@ -11,9 +11,9 @@ public class WithdrawDto {
     @Getter
     public static class Request {
 
-        @NotBlank
+        @NotBlank(message = "계좌번호는 필수값입니다.")
         private String accountNumber;
-        @NotBlank
+        @NotBlank(message = "출금자명은 필수값입니다.")
         private String withdrawName;
         @Min(value = 1000, message = "출금 최소금액은 1000원입니다.")
         private Long amount;
