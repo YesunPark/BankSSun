59445c4d02285677b1f0457b227e8f03319081d9
YesunPark <lynn08082@gmail.com>
2023-08-02 15:40:20 +0900

    Fix: 현금 입금 최소값 추가, 에러메세지 추가

--- src/main/java/com/zerobase/BankSSun/dto/transaction/DepositDto.java
+++ src/main/java/com/zerobase/BankSSun/dto/transaction/DepositDto.java
@@ -1,6 +1,7 @@
 package com.zerobase.BankSSun.dto.transaction;
 
 import java.time.LocalDateTime;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,11 +11,11 @@ public class DepositDto {
     @Getter
     public static class Request {
 
-        @NotBlank
+        @NotBlank(message = "계좌번호는 필수값입니다.")
         private String accountNumber;
-        @NotBlank
+        @NotBlank(message = "입금자명은 필수값입니다.")
         private String depositName;
-//        @Size(min = 1)
+        @Min(value = 1000, message = "입금 최소금액은 1000원입니다.")
         private Long amount;
     }
 
